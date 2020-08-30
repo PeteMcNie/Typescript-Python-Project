e29bd5f69f6413e00fa852d6a2596599bc525558
petemcnie <petemcnie0@gmail.com>
2020-08-31 10:17:23 +1200

    Initial commit

---
+++ client/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import { Route } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <>
+      <h1>hello world</h1>
+    </>
+  )
+}
+
+export default App
---
+++ client/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux'
+
+// import newTeams from './twoTeamsReducer'
+
+export default combineReducers({
+//   newTeams
+})
