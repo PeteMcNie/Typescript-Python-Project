d7bc5b2c2fe8ae6accb9700abbe7566adfc5e33e
petemcnie <petemcnie0@gmail.com>
2020-09-29 15:20:05 +1300

    Add action functions

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -24,3 +24,38 @@ export interface RemoveTeamName {
     type: constants.REMOVE_TEAM_NAME
     teamName: string
 }
+
+//Define the actions
+export function addPerson(person: string): AddPerson {
+    return {
+    type: constants.ADD_PERSON,
+    name: person
+    }
+}
+
+export function removerPerson(person: string): RemovePerson {
+    return {
+        type: constants.REMOVE_PERSON,
+        name: person
+    }
+}
+
+export function numberOfTeams(): NumberOfTeams {
+    return {
+        type: constants.NUMBER_OF_TEAMS
+    }
+}
+
+export function addTeamName(teamName: string): AddTeamName {
+    return {
+        type: constants.ADD_TEAM_NAME,
+        teamName: teamName
+    }
+}
+
+export function removeTeamName(teamName: string): RemoveTeamName {
+    return {
+        type: constants.REMOVE_TEAM_NAME,
+        teamName: teamName
+    }
+}
