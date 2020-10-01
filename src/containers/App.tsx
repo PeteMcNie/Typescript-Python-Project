684d5e0a5a93aca4d09899aa2cc55648b2a47b27
petemcnie <petemcnie0@gmail.com>
2020-10-02 12:07:09 +1300

    Add container for mapStateToProps

---
+++ src/containers/App.tsx
@@ -0,0 +1,14 @@
+import App from '../components/App'
+import * as actions from '../actions'
+import { StoreState } from '../types/index'
+import { connect } from 'react-redux'
+
+export function mapStateToProps ({people, noOfTeams, teamNames}: StoreState) {
+    return {
+        people,
+        noOfTeams,
+        teamNames
+    }
+}
+
+export default connect(mapStateToProps)(App)
