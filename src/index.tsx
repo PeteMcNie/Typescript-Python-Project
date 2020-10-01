6441f2f666f22c2c08631d6163810da9541fb769
petemcnie <petemcnie0@gmail.com>
2020-10-02 11:19:14 +1300

    Store created

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,22 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
+import { createStore } from 'redux'
+import { people } from './reducers/index'
+import { StoreState } from './types/index'
+import { Provider } from 'react-redux'
+
 import App from './components/App'
 
+const store = createStore<StoreState>(people, { 
+  people: ['Pete', 'Nigel'],
+  noOfTeams: 2,
+  teamNames: ['Blue', 'Red']
+})
+
 ReactDOM.render(
-  <App userName="Developer" lang="TypeScript" />,
-  document.getElementById('output'),
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root') as HTMLElement
 )
