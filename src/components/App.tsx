0cabf13a8c6d0ab3a224bd3f6221941e60affa54
petemcnie <petemcnie0@gmail.com>
2020-10-02 15:38:04 +1300

    Add onchange and onclick handlers, data being passed to reducer

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,31 +1,49 @@
-import * as React from 'react'
-// import * as actions from '../actions'
+import React, { useState } from 'react'
+import * as actions from '../actions'
 import { StoreState } from '../types/index'
-import { connect } from 'react-redux'
+import { connect, Dispatch } from 'react-redux'
 
 export interface Props {
-  people: string
+  people: string[]
   noOfTeams: number
   teamNames: string
+  submitPerson(person: string | undefined): () => void
 }
 
-function App ({ people, noOfTeams, teamNames }: Props) {
+function App ({ people, noOfTeams = 4, teamNames, submitPerson }: Props) {
+    // const [statePerson, setPersonState] = useState('')
+    const [statePerson, setPersonState] = useState<string | undefined>(undefined)
+
+    function handleChange (evt) {
+        console.log(evt.target.value)
+        const { value }: { value: string } = evt.target
+        setPersonState(value)
+    }
+
     console.log('App ', people, noOfTeams, teamNames)
     return (
         <div>
             <h1>GOOD MORNING</h1>
             <p>{noOfTeams}</p>
-            <p>{people}</p>
+            {people.map(el => {
+              return (
+                <div key={el}>
+                  <p>{el}</p>
+                </div>
+                    )
+                }
+            )}
+            <p>{teamNames}</p>
             <div>
-                <input />
-                <button>Submit</button>
+                <input onChange={handleChange} value={statePerson}/>
+                <button onClick={() => submitPerson(statePerson)}>Submit</button>
             </div>
         </div>
 
     )
 }
 
-function mapStateToProps (state: StoreState, ownProps: Props) {
+function mapStateToProps (state: StoreState) {
     return {
         people: state.people,
         noOfTeams: state.noOfTeams,
@@ -33,4 +51,10 @@ function mapStateToProps (state: StoreState, ownProps: Props) {
     }
 }
 
-export default connect(mapStateToProps)(App)
+function mapDispatchToProps (dispatch: Dispatch<actions.UpdatePerson>) {
+    return {
+        submitPerson: (person) => dispatch(actions.addPerson(person))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
